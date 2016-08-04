79db9d1b99dd0feb80f33de92bd7de6bf59aafe4
marc <marcmarcwongwong@gmail.com>
2016-08-03 21:31:42 -0700

    Wrong stream

--- js/twitchtv.js
+++ js/twitchtv.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 //The Array of Twitch Streams
-var twitchStreams = ["ESL_SC2", "OgamingSC2", "mega64podcast", "rogersbase", "freecodecamp", "storbeck", "maximilian_dood", "kingoflightninggaming", "noobs2ninjas","cashnastygaming","testtest","brunofin","comster404"];
+var twitchStreams = ["ESL_SC2", "OgamingSC2", "mega64podcast", "rogersbase", "freecodecamp", "storbeck", "maximilian_dood", "kingoflightninggaming", "noobs2ninjas","cashnastygaming","testingapi","brunofin","comster404"];
 
 //Ignore:
 var streamArray = [];
